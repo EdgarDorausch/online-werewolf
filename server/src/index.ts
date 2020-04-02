87821744b8fcf2c061f09c7187710eef66419d2f
Edgar Dorausch <EdgarDorausch@web.de>
2020-04-02 20:21:11 +0200

    Add Socket io connection handler

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,24 @@
 import * as express from 'express';
+import * as socketio from 'socket.io';
 
 const app = express();
 app.set("port", process.env.PORT || 8000);
 
-var http = require("http").Server(app);
+const http = require("http").Server(app);
+const io = require('socket.io')(http)
 
 // simple '/' endpoint sending a Hello World
 // response
 app.get("/", (req: any, res: any) => {
   res.send("hello world");
+});
+
+io.on("connection", function(socket: any) {
+  console.log("a user connected");
+  // whenever we receive a 'message' we log it out
+  socket.on("message", function(message: any) {
+    console.log(message);
+  });
 });
 
 // start our simple server up on localhost:3000
